cbfe8061ad2581ced2b9ebff5485fba45d701f74
Charles Cruse <charle9@vt.edu>
2024-03-10 22:27:03 -0400

    added create issue functionality to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,19 @@ function App() {
     fetchIssues()
   }, [])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const maxId = issues.reduce((max, issue) => {
+      return issue.id > max ? issue.id : max;
+    }, 0)
+
+    const id = maxId + 1
+    const newIssueWithId = { id, ...newIssue }
+    await axios.post('http://localhost:5000/api/issues', newIssueWithId)
+    setNewIssue({ title: '', description: '' })
+    fetchIssues()
+  }
+
   const handleDelete = async (id) => {
     await axios.delete(`http://localhost:5000/api/issues/${id}`)
     fetchIssues()
@@ -26,11 +39,25 @@ function App() {
       <h1>Current Issues</h1>
       {issues.map((issue) => (
         <div key={issue.id}>
-          <span>{issue.title} - {issue.description}</span>
+          <span>{issue.id} - {issue.title} - {issue.description}</span>
           <button>Edit</button>
           <button onClick={() => handleDelete(issue.id)}>Delete</button>
         </div>
       ))}
+      <h2>Create an Issue</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder='Title'
+          value={newIssue.title}
+          onChange={(e) => setNewIssue({ ...newIssue, title: e.target.value })}
+        />
+        <input
+          placeholder="Description"
+          value={newIssue.description}
+          onChange={(e) => setNewIssue({ ...newIssue, description: e.target.value })}
+        />
+        <button type="submit">Create Issue</button>
+      </form>
     </>
   )
 }

968bac0f457cec4b5106aec7bbb92b629e811165
Charles Cruse <charle9@vt.edu>
2024-03-10 21:53:28 -0400

    Added boilerplate code for React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [issues, setIssues] = useState([])
+
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
